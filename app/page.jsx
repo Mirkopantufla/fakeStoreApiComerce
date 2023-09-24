996ab759d95244e50fd89725814d29b4654f2d0f
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2023-09-23 23:54:35 -0300

    Imagen añadida

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='grid-cols-12'>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          <Image
+            src="/disco-duro.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+            width={500}
+            height={500}
+            alt="HardDrive" />
           <div>
             <h1 className="text-5xl font-bold">Fake Store News!</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
