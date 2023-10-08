f60201b56a22d1a685844106c74b4a2e0434a4ea
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2023-10-07 23:43:20 -0300

    Añadida clasificacion en la parte especificacion del producto

--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -13,15 +13,21 @@ const ProductDisplay = async ({ params }) => {
     const product = await fetchProduct(params.productId)
 
     return (
-        <div className='grid grid-cols-6 grid-rows-3 justify-center min-h-screen min-w-full'>
+        <div className='grid grid-cols-6 grid-rows-3 justify-center items-center min-h-screen min-w-full'>
             <div className='grid-start-1 col-span-1'>
                 <ProductClientButton />
             </div>
-            <div className='col-start-2 col-span-4 row-span-3 flex flex-col items-center'>
-                <img style={{ width: "400px" }} src={product.image} alt="" />
+            <div className='col-start-2 col-span-4 row-span-3 flex flex-col items-center border-2 p-10'>
+                <img style={{ width: "400px", height: "400px" }} src={product.image} alt="" />
                 {product.id}
                 <h1 className='text-4xl'>{product.title}</h1>
                 <h1>{product.price}</h1>
+                <br />
+                <div className='flex flex-col items-center'>
+                    <label htmlFor="">Clasificación</label>
+                    <p className='text-xl'>{product.rating.rate}</p>
+                    <progress className="progress progress-primary w-56" value={(product.rating.rate * 10) * 2} max="100"></progress>
+                </div>
             </div>
         </div>
     )
