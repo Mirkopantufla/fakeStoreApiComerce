bd5e0e8292a6a0f15478a453b6b017ee617539b9
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2023-12-13 23:47:22 -0300

    Carrito empezado!

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Cart = () => {
     return (
-        <div>Cart</div>
+        <div className='flex flex-col justify-center items-center'>
+            Administrador de Carro
+            <div>
+                <button className='btn btn-primary'>Hola mundo</button>
+            </div>
+        </div>
     )
 }
 
