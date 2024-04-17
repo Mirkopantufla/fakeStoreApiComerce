ff958b66ba54a36c91a0fb06ae7625ab32cad3d3
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-16 23:09:12 -0400

    Restructure: modificada la vista del layout de administrador para mejor ux

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,31 +1,33 @@
 'use client'
-import Link from 'next/link'
 import React from 'react'
 import IsAdmin from '@/components/IsAdmin'
+import NavLink from '@/components/NavLink'
+
 
 export const metadata = {
     title: 'Admin Fake Store',
     description: 'Fake store with nextJS, data from fakeStoreAPI',
 }
 
-const liClass = 'outline outline-1 bg-neutral text-primary hover:bg-primary hover:text-neutral font-bold cursor-pointer w-full h-[10vh] p-0'
-const linkClass = 'flex justify-center items-center w-full h-full m-0';
+const linkClass = 'flex justify-center items-center font-bold cursor-pointer outline outline-1 h-16';
 
 const AdminLayout = ({ children }) => {
 
     return (
-        <>
-            <div className='h-[15vh] flex flex-col justify-between'>
-                <h1 className='text-center text-4xl pt-5'>Admin Section</h1>
-                <ul className='flex bg-opacity-25'>
-                    <li className={`${liClass} `}><Link className={linkClass} href={'/admin/products'}>Products</Link></li>
-                    <li className={`${liClass} `}><Link className={linkClass} href={'/admin/offers'}>Offers</Link></li>
-                    <li className={liClass}><Link className={linkClass} href={'/admin/users'}>Users</Link></li>
-                </ul>
 
+        <div className='grid grid-cols-8 min-h-[76vh]'>
+            <div className='col-span-1 p-2 border border-neutral border-y-0'>
+                <ul className='bg-opacity-25'>
+                    <li><NavLink classes={linkClass} href={'/admin/users'} text={'Users'} /></li>
+                    <li><NavLink classes={linkClass} href={'/admin/products'} text={'Products'} /></li>
+                    <li><NavLink classes={linkClass} href={'/admin/offers'} text={'Offers'} /></li>
+                </ul>
             </div>
-            {children}
-        </>
+            <div className='col-span-7'>
+                {children}
+            </div>
+        </div>
+
     )
 }
 
