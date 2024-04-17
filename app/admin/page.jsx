8bf9c1b836a3da3732e948b64663fa01448e63ab
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-16 23:10:08 -0400

    Restructure: Modificado el texto en negro, ya que al cambiar a modo light este no se ve.

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const adminPage = () => {
 
     return (
-        <div className='min-h-[60vh] flex flex-col justify-evenly'>
-            <h2 className='text-center text-3xl py-3 bg-neutral'>Aqui puedes gestionar tus usuarios y productos</h2>
+        <div className='flex flex-col justify-center p-20 h-full'>
+            <h1 className='text-center text-4xl font-bold p-3'>Admin Section</h1>
+            <h2 className='text-center text-3xl py-3 bg-neutral text-primary'>Here you can administrate your page</h2>
         </div>
     )
 }
