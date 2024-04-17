a69bbee6eb746dd231ac1511376945fba33cb70e
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-16 23:11:23 -0400

    Restructure: Modificada la estructura el menu de productos para mejor ux

---
+++ app/admin/products/layout.jsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+import IsAdmin from '@/components/IsAdmin'
+import NavLink from '@/components/NavLink'
+
+
+export const metadata = {
+    title: 'Admin Fake Store',
+    description: 'Fake store with nextJS, data from fakeStoreAPI',
+}
+
+const ProductLayout = ({ children }) => {
+
+    const linkClass = 'flex justify-center items-center font-bold cursor-pointer outline outline-1 h-full';
+
+    return (
+
+        <div className='flex flex-col h-full'>
+            <div className='h-16'>
+                <ul className='flex justify-between bg-opacity-25 m-2 gap-2 h-12'>
+                    <li className='w-1/3'><NavLink classes={linkClass} text={'List'} href={'/admin/products/list'}></NavLink></li>
+                    <li className='w-1/3'><NavLink classes={linkClass} text={'Search'} href={'/admin/products/search'}></NavLink></li>
+                    <li className='w-1/3'><NavLink classes={linkClass} text={'Add'} href={'/admin/products/add'}></NavLink></li>
+                </ul>
+            </div>
+
+            {children}
+
+        </div>
+
+    )
+}
+
+export default IsAdmin(ProductLayout)
