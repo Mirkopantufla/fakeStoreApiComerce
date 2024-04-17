ae594e6859483cd4b7a020d7ece1f795274f765b
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-16 23:07:46 -0400

    Update: Modificado metodo de actividad de los links para una mejor ux

---
+++ components/NavLink.jsx
@@ -0,0 +1,26 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation"
+
+const NavLink = ({ href, text, classes }) => {
+    const path = usePathname()
+    const activeClasses = 'bg-primary text-neutral hover:bg-neutral hover:text-primary';
+    const inactiveClasses = 'bg-neutral text-primary hover:bg-primary hover:text-neutral';
+    // De las rutas, separar por / y saltar los primeros dos que son "" y admin
+    let auxPathArray = path.split("/")
+    auxPathArray = auxPathArray.splice(2)
+
+    //Toma la ultima posición del href definido en el link, y lo almacena en lastPosition
+    let lastPosition = href.split("/")
+    lastPosition = lastPosition.pop()
+
+    // Comparo ambos dos arrays que contienen las rutas 
+    // Si existe en el array path alguno la ultima posicion almacenada en lastPosition, significa que estamos dentro de este enlace.
+    const isActive = auxPathArray.includes(lastPosition);
+
+    return (
+        <Link className={`${classes} ${isActive ? activeClasses : inactiveClasses}`} href={href}>{text}</Link>
+    )
+}
+
+export default NavLink
