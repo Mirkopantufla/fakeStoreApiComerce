aca21fee6ebb46e3cbde8ac5b4ed3968f8da4472
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-16 23:11:58 -0400

    Restructure: Agregado mensaje para indicar donde esta ubicado el usuario

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -1,17 +1,12 @@
-import Link from 'next/link'
 import React from 'react'
 
 const AdminProducts = () => {
-
-    const liClass = 'outline outline-1 bg-neutral text-primary rounded-md hover:bg-primary hover:text-neutral font-bold cursor-pointer w-full h-[45vh] p-0 m-10'
-    const linkClass = 'flex justify-center items-center text-5xl w-full h-full m-0';
-
     return (
 
-        <ul className='flex bg-opacity-25 m-10'>
-            <li className={liClass}><Link className={linkClass} href={'/admin/products/search'}>Search</Link></li>
-            <li className={liClass}><Link className={linkClass} href={'/admin/products/add'}>Add</Link></li>
-        </ul>
+        <div className='flex flex-col justify-center p-20 h-full'>
+            <h1 className='text-center text-4xl font-bold p-3'>Product Section</h1>
+            <h2 className='text-center text-3xl py-3 bg-neutral text-primary'>Here you can administrate your products</h2>
+        </div>
 
     )
 }
