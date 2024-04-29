ea46da75839d97ac1efd22fdfdbc47c1cfc50113
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-04-29 12:47:15 -0400

    Refactor: refactorizado el codigo para ahorrar lineas de codigo

--- utils/auth.js
+++ utils/auth.js
@@ -16,19 +16,10 @@ export function isAdministrator() {
     let user = undefined;
     //Verifico que el componente este montado antes de consutlar por el local storage
     //Ya que por propiedades de nextJS no se puede consultar al local storage antes de montar el componente
-    if (typeof window !== 'undefined') {
-        user = JSON.parse(localStorage.getItem('user'));
-    }
 
-    if (user) {
-        if (user.role_id === 1) {
-            return true
-        } else {
-            return false
-        }
-    } else {
-        return false
-    }
+    user = JSON.parse(localStorage.getItem('user'));
+
+    return user && user.role_id === 1
 
 }
 
